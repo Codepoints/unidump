5312ce2d9828dacb4d532518c310a7c825613371
Manuel Strehl <git@manuel-strehl.de>
2017-03-22 22:15:07 +0100

    make l10n-ready

--- unidump/cli.py
+++ unidump/cli.py
@@ -4,73 +4,88 @@ import sys
 from unidump import version, unidump
 from unidump.env import env
 from unicodedata import unidata_version
+import gettext
+from os.path import dirname
+from textwrap import TextWrapper
+from shutil import get_terminal_size
 
 
-description = '''  A Unicode code point dump.
+gettext.install('unidump', localedir=dirname(__file__)+'/locale')
+tw = TextWrapper(width=min(80, get_terminal_size().columns),
+        replace_whitespace=True,
+        initial_indent='  ', subsequent_indent='  ').fill
 
-  Think of it as  hexdump(1)  for Unicode.  The command analyses  the input and
-  then prints three columns: the raw byte index of the first code point in this
-  row, code points in their hex notation,  and finally the raw input characters
-  with control and whitespace replaced by a dot.
 
-  Invalid byte sequences are represented with an “X” and with the hex value en-
-  closed in question marks, e.g., “?F5?”.
+description = '\n\n'.join([
+  tw(_('A Unicode code point dump.')),
 
-  You can pipe in  data from stdin,  select several files at once,  or even mix
-  all those input methods together.'''
+  tw(_('Think of it as hexdump(1) for Unicode. The command analyses the input '
+      'and then prints three columns: the raw byte index of the first code point '
+      'in this row, code points in their hex notation, and finally the raw input '
+      'characters with control and whitespace replaced by a dot.')),
 
-epilog = '''Examples:
+  tw(_('Invalid byte sequences are represented with an “X” and with the hex '
+     'value enclosed in question marks, e.g., “?F5?”.')),
 
-* Basic usage with stdin:
+  tw(_('You can pipe in data from stdin, select several files at once, or '
+      'even mix all those input methods together.')),
+])
 
-      echo -n 'ABCDEFGHIJKLMNOP' | unidump -n 4
+epilog = '\n\n'.join([
+    _('Examples:'),
+
+    tw(_('* Basic usage with stdin:')),
+
+    '''      echo -n 'ABCDEFGHIJKLMNOP' | unidump -n 4
             0    0041 0042 0043 0044    ABCD
             4    0045 0046 0047 0048    EFGH
             8    0049 004A 004B 004C    IJKL
-           12    004D 004E 004F 0050    MNOP
+           12    004D 004E 004F 0050    MNOP''',
 
-* Dump the code points translated from another encoding:
+    tw(_('* Dump the code points translated from another encoding:')),
 
-      unidump -c latin-1 some-legacy-file
+    '      unidump -c latin-1 some-legacy-file',
 
-* Dump many files at the same time:
+    tw(_('* Dump many files at the same time:')),
 
-      unidump foo-*.txt
+    '      unidump foo-*.txt',
 
-* Control characters and whitespace are safely rendered:
+    tw(_('* Control characters and whitespace are safely rendered:')),
 
-      echo -n -e '\\x01' | unidump -n 1
-           0    0001    .
+    '''      echo -n -e '\\x01' | unidump -n 1
+           0    0001    .''',
 
-* Finally learn what your favorite Emoji is composed of:
+    tw(_('* Finally learn what your favorite Emoji is composed of:')),
 
-      ( echo -n -e '\\xf0\\x9f\\xa7\\x9d\\xf0\\x9f\\x8f\\xbd\\xe2' ; \\
+    '''      ( echo -n -e '\\xf0\\x9f\\xa7\\x9d\\xf0\\x9f\\x8f\\xbd\\xe2' ; \\
         echo -n -e '\\x80\\x8d\\xe2\\x99\\x82\\xef\\xb8\\x8f' ; ) | \\
       unidump -n 5
-           0    1F9DD 1F3FD 200D 2642 FE0F    .🏽.♂️
+           0    1F9DD 1F3FD 200D 2642 FE0F    .🏽.♂️''',
 
-  See  <http://emojipedia.org/man-elf-medium-skin-tone/> for images.  The “elf”
-  emoji (the first character) is replaced with a dot here,  because the current
-  version of Python’s unicodedata doesn’t know of this character yet.
+    tw(_('See <http://emojipedia.org/man-elf-medium-skin-tone/> for images. '
+         'The “elf” emoji (the first character) is replaced with a dot here, '
+         'because the current version of Python’s unicodedata doesn’t know of '
+         'this character yet.')),
 
-* Use it like strings(1):
+    tw(_('* Use it like strings(1):')),
 
-      unidump -e '{data}' some-file.bin
+    '      unidump -e \'{data}\' some-file.bin',
 
-  This will replace  every unknown byte from the input file  with “X” and every
-  control and whitespace character with “.”.
+    tw(_('This will replace every unknown byte from the input file with “X” '
+         'and every control and whitespace character with “.”.')),
 
-* Only print the code points of the input:
+    tw(_('* Only print the code points of the input:')),
 
-      unidump -e '{repr}'$'\\n' -n 1 some-file.txt
+    '''      unidump -e '{repr}'$'\\n' -n 1 some-file.txt''',
 
-  This results in a stream of code points in hex notation,  each on a new line,
-  without byte counter  or rendering of actual data.  You can use this to count
-  the total amount of characters  (as opposed to raw bytes)  in a file,  if you
-  pipe it through `wc -l`.
-'''+'''
-This is version {} of unidump, using Unicode {} data.
-'''.format(version, unidata_version)
+    tw(_('This results in a stream of code points in hex notation, each on a '
+         'new line, without byte counter or rendering of actual data. You can '
+         'use this to count the total amount of characters (as opposed to raw '
+         'bytes) in a file, if you pipe it through `wc -l`.')),
+
+    tw(_('This is version {} of unidump, using Unicode {} data.')
+    .format(version, unidata_version)).lstrip() + '\n'
+])
 
 
 def main(args=None):
@@ -88,25 +103,28 @@ def main(args=None):
     )
 
     parser.add_argument('files', nargs='*', metavar='FILE', default=('-',),
-                        help='input files. Use `-\' or keep empty for stdin.')
+                        help=_(
+                            'input files. Use “-” or keep empty for stdin.'))
     parser.add_argument('-n', '--length', type=int, default=16,
                         dest='linelength', metavar='LENGTH',
-                        help='format output using this much input characters. '
-                        'Default is %(default)s characters.')
+                        help=_(
+                            'format output using this much input characters. '
+                            'Default is %(default)s characters.'))
     parser.add_argument('-c', '--encoding', type=str, default='utf-8',
                         metavar='ENC',
-                        help='interpret input in this encoding. Default is '
-                        '%(default)s. You can choose any encoding that Python '
-                        'supports, e.g. “latin-1”.')
+                        help=_(
+                            'interpret input in this encoding. Default is '
+                            '%(default)s. You can choose any encoding that '
+                            'Python supports, e.g. “latin-1”.'))
     parser.add_argument('-e', '--format', type=str, default=None,
                         dest='lineformat', metavar='FORMAT',
-                        help=(
+                        help=_(
                             'specify a custom format in Python’s {} notation. '
                             'Default is “%s”. '
                             'See examples below on how to use this option.'
                         ) % env.lineformat.replace('\n', '\\n'))
     parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s {} using Unicode {} data'.format(
+                        version=_('%(prog)s {} using Unicode {} data').format(
                             version, unidata_version))
 
     a = parser.parse_args(args)
@@ -120,11 +138,13 @@ def main(args=None):
                     infile = open(filename, 'rb')
                 except FileNotFoundError:
                     sys.stdout.flush()
-                    sys.stderr.write('File {} not found.\n'.format(filename))
+                    sys.stderr.write(_('File {} not found.\n')
+                                     .format(filename))
                     continue
                 except IsADirectoryError:
                     sys.stdout.flush()
-                    sys.stderr.write('{} is a directory.\n'.format(filename))
+                    sys.stderr.write(_('{} is a directory.\n')
+                                     .format(filename))
                     continue
             unidump(infile, env=env(linelength=a.linelength,
                     encoding=a.encoding, lineformat=a.lineformat,
